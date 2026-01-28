7574ed0da72f2105ead7b9499b2c2567d0ecf761
Anuragkumar6696 <officialanurag6696@gmail.com>
2026-01-28 02:09:25 -0800

    upload adpi added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { connect } = require("./config/db");
 // const subscriberRoutes = require("./routes/subscriber.route");
 const leadRoutes = require("./routes/leads.route");
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // app.use("/", subscriberRoutes);
 app.use("/api/lead", leadRoutes);
